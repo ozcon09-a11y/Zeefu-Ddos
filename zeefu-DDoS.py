e3bfb8bced01cc1f69270764075ae059cdd6e9db
Ozcon99 <ozcon09@gmail.com>
2025-08-25 13:49:09 +0700

    Update zeefu-DDoS.py

--- zeefu-DDoS.py
+++ zeefu-DDoS.py
@@ -102,7 +102,7 @@ banner = f"""
 {Fore.YELLOW}║{Fore.GREEN}                   created to depend PALESTINE..!                        {Fore.YELLOW}║
 {Fore.YELLOW}║{Fore.BLUE}                  T H E  B L E S S E D  C O U N T R Y                    {Fore.YELLOW}║
 {Fore.YELLOW}║{Fore.WHITE}                          Created By: Ozcon                              {Fore.YELLOW}║
-{Fore.YELLOW}║{Fore.LIGHTYELLOW_EX}                          ——oO0Oo——                                    {Fore.YELLOW}║
+{Fore.YELLOW}║{Fore.LIGHTYELLOW_EX}                               ——oO0Oo——                              {Fore.YELLOW}║
 {Fore.YELLOW}╚═════════════════════════════════════════════════════════════════════════╝
 ===>> Z E E F U MEMBUTUHKAN 35 DETIK UNTUK MEMULAI SERANGAN...!! <==={Fore.RESET}"""
 
