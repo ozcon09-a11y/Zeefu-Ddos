288ce85afbac7713a4ab453de2562d0006fdf3f5
Ozcon99 <ozcon09@gmail.com>
2025-08-25 11:53:39 +0700

    Update zeefu-DDoS.py

--- zeefu-DDoS.py
+++ zeefu-DDoS.py
@@ -95,7 +95,8 @@ banner = f"""
 {Fore.WHITE}    ██▒▒           ██▒▒           ██▒▒            ██▒▒             ██▒▒      ██▒▒
 {Fore.WHITE}  ██▒██▒██▒██▒▒  ██▒██▒██▒██▒▒ ██▒██▒██▒██▒▒ ██▒▒                 ██▒██▒▒
 {Fore.YELLOW}   ▒▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒                   ▒▒▒▒▒▒  
-{fore.WHITE}      ▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒      ▒▒                     ▒▒▒
+{Fore.WHITE}      ▒▒▒▒▒▒▒▒        ▒▒▒▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒      ▒▒                     ▒▒▒
+{Fore.CYAN}         ▒▒▒▒▒            ▒▒▒▒▒         ▒▒▒▒▒         ▒                      ▒
 {Fore.YELLOW}╔═══════════════════════════════════════════════════════════════╗
 {Fore.YELLOW}║{Fore.WHITE}                H U D A I R U L  A L - A Q S H A               {Fore.YELLOW}║
 {Fore.YELLOW}║{Fore.GREEN}                         INTERNAL SCRIPT                       {Fore.YELLOW}║
